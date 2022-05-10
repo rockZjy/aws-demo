7fb12430c1dea9ba7f908e43adce883eccce0782
rockZjy <36514239@qq.com>
2022-05-10 19:49:01 +0800

    code init

---
+++ aws-sp-demo/src/main/java/com/example/aws/sp/AwsSpDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.aws.sp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsSpDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsSpDemoApplication.class, args);
+	}
+
+}
---
+++ aws-sp-demo/src/test/java/com/example/aws/sp/AwsSpDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aws.sp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsSpDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
